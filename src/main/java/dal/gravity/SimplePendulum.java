4757fa4cbc36ec4b41ef82d0873de0fd0e9bfced
damienr74 <damienr74@gmail.com>
2017-04-04 02:22:51 -0300

    Apply the collapse Hierarchy pattern

--- src/main/java/dal/gravity/SimplePendulum.java
+++ src/main/java/dal/gravity/SimplePendulum.java
@@ -3,37 +3,41 @@ package dal.gravity;
 /**
  * Represents pendulums exhibiting (approximately) simple harmonic motion
  */
-public class SimplePendulum extends AbstractEarthPendulum {
+public class SimplePendulum extends AbstractPendulum {
 
-    private double angularFrequency, periodOfMotion;
+	private double angularFrequency, periodOfMotion;
 
-    /**
-     * Creates a new Pendulum instance using
-     * inLength: the string length (>0)
-     * inMass: the point mass (>0)
-     * inTheta0: angular displacement at t=0 (0<=theta0<=pi/6)
-     */
-    public SimplePendulum (double inLength, double inMass, double inTheta0) {
-	super (inLength, inMass, inTheta0);
-	angularFrequency = Math.sqrt (this.getGravitationalField () / this.getStringLength ());
-	periodOfMotion = 2 * Math.PI 
-	    * Math.sqrt (this.getStringLength () / this.getGravitationalField ());
-    }
+	/**
+	 * Creates a new Pendulum instance using
+	 * inLength: the string length (>0)
+	 * inMass: the point mass (>0)
+	 * inTheta0: angular displacement at t=0 (0<=theta0<=pi/6)
+	 */
+	public SimplePendulum(double inLength, double inMass, double inTheta0) {
+		super(inLength, inMass, inTheta0, EARTH_GRAVITY);
 
-    /**
-     * provides this Pendulum's angular frequency
-     */ 
-    public double getAngularFrequency () { return angularFrequency; }
+		angularFrequency = Math.sqrt(this.getGravitationalField() /
+				this.getStringLength());
 
-    /**
-     * provides this Pendulum's period of motion
-     */ 
-    public double getPeriodOfMotion () { return periodOfMotion; }
+		periodOfMotion = 2 * Math.PI * Math.sqrt(this.getStringLength() /
+				this.getGravitationalField());
+	}
 
-    /**
-     * provides the angular displacement of this Pendulum at time t
-     */
-    public double getTheta (double t) {
-	return this.getMaxAngularDisplacement () * Math.cos (angularFrequency * t);
-    }
+	/**
+	 * provides this Pendulum's angular frequency
+	 */
+	public double getAngularFrequency() { return angularFrequency; }
+
+	/**
+	 * provides this Pendulum's period of motion
+	 */
+	public double getPeriodOfMotion() { return periodOfMotion; }
+
+	/**
+	 * provides the angular displacement of this Pendulum at time t
+	 */
+	public double getTheta(double t) {
+		return this.getMaxAngularDisplacement() *
+			Math.cos(angularFrequency * t);
+	}
 }
